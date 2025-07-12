c3c84e802b26dc9c16a9136ff6b852d5e5a31a09
kumarpalsinh25 <info@dajiraj.com>
2025-07-12 14:17:05 +0530

    Update content_block_widget.dart

--- lib/widgets/content_block_widget.dart
+++ lib/widgets/content_block_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../common/models/content_block.dart';
-import '../common/theme/app_theme.dart';
 
 class ContentBlockWidget extends StatefulWidget {
   final ContentBlock block;
