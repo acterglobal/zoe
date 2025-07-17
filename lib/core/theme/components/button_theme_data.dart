6741b0782a4d30ede8e6d9ba8268b4a24e463af0
kumarpalsinh25 <info@dajiraj.com>
2025-07-17 14:05:01 +0530

    Update button_theme_data.dart

--- lib/core/theme/components/button_theme_data.dart
+++ lib/core/theme/components/button_theme_data.dart
@@ -4,7 +4,7 @@ ElevatedButtonThemeData elevatedButtonTheme(ColorScheme colorScheme) =>
     ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         elevation: 0,
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        padding: const EdgeInsets.all(20),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         backgroundColor: colorScheme.primary,
         foregroundColor: Colors.white,
