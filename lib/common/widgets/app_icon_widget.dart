30869d554e56256c9a8498e1defe85fac9951f92
kumarpalsinh25 <info@dajiraj.com>
2025-07-25 18:57:59 +0530

    Added shimmer effect and custom app icon

--- lib/common/widgets/app_icon_widget.dart
+++ lib/common/widgets/app_icon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoey/common/widgets/shimmer_overlay_widget.dart';
 
 class AppIconWidget extends StatelessWidget {
   final double size;
@@ -6,14 +7,57 @@ class AppIconWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.primary,
-        borderRadius: BorderRadius.circular(20),
+    final primaryColor = Theme.of(context).colorScheme.primary;
+    final secondaryColor = Theme.of(context).colorScheme.secondary;
+    final borderRadius = BorderRadius.circular(size * 0.25);
+
+    return ShimmerOverlay(
+      borderRadius: borderRadius,
+      shimmerColors: [
+        Colors.transparent,
+        Colors.white.withValues(alpha: 0.15),
+        Colors.white.withValues(alpha: 0.35),
+        Colors.white.withValues(alpha: 0.15),
+        Colors.transparent,
+      ],
+      duration: const Duration(seconds: 2),
+      child: Container(
+        width: size,
+        height: size,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: borderRadius,
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              primaryColor,
+              primaryColor.withValues(alpha: 0.8),
+              secondaryColor.withValues(alpha: 0.6),
+              secondaryColor,
+            ],
+            stops: const [0.0, 0.3, 0.7, 1.0],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: primaryColor.withValues(alpha: 0.3),
+              blurRadius: size * 0.15,
+              offset: Offset(0, size * 0.05),
+              spreadRadius: size * 0.01,
+            ),
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.1),
+              blurRadius: size * 0.2,
+              offset: Offset(0, size * 0.1),
+            ),
+          ],
+        ),
+        child: Icon(
+          Icons.auto_stories_rounded,
+          size: size * 0.5,
+          color: Colors.white,
+        ),
       ),
-      child: Icon(Icons.note_alt_rounded, size: size / 2, color: Colors.white),
     );
   }
 }
