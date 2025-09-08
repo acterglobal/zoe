5f82d4a293d25971e96bb7d7efb7de3709f5f171
anisha-e10 <anisha.e10@dajiraj.com>
2025-09-08 17:40:19 +0530

    removed welcome screen features view

--- lib/features/welcome/screens/welcome_screen.dart
+++ lib/features/welcome/screens/welcome_screen.dart
@@ -5,8 +5,6 @@ import 'package:zoe/common/widgets/animated_background_widget.dart';
 import 'package:zoe/common/widgets/app_icon_widget.dart';
 import 'package:zoe/common/widgets/toolkit/zoe_primary_button.dart';
 import 'package:zoe/core/routing/app_routes.dart';
-import 'package:zoe/features/welcome/data/feature_data.dart';
-import 'package:zoe/features/welcome/widgets/feature_item.dart';
 import 'package:zoe/l10n/generated/l10n.dart';
 
 class WelcomeScreen extends ConsumerStatefulWidget {
@@ -22,12 +20,10 @@ class _WelcomeScreenState extends ConsumerState<WelcomeScreen> {
     return Scaffold(
       body: AnimatedBackgroundWidget(
         child: SafeArea(
-          child: Center(
-            child: Container(
-              constraints: BoxConstraints(maxWidth: 600),
-              padding: const EdgeInsets.all(24),
-              child: _buildWelcomeBodyUI(),
-            ),
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 600),
+            padding: const EdgeInsets.all(24),
+            child: _buildWelcomeBodyUI(),
           ),
         ),
       ),
@@ -36,41 +32,16 @@ class _WelcomeScreenState extends ConsumerState<WelcomeScreen> {
 
   Widget _buildWelcomeBodyUI() {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        const AppIconWidget(),
         const SizedBox(height: 32),
-        Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const AppIconWidget(),
-                const SizedBox(height: 32),
-                _buildAppTitle(),
-                const SizedBox(height: 8),
-                _buildAppDescription(),
-                const SizedBox(height: 48),
-                _buildFeaturesList(),
-              ],
-            ),
-          ),
-        ),
+        _buildAppTitle(),
+        const SizedBox(height: 8),
+        _buildAppDescription(),
         const SizedBox(height: 32),
         _buildGetStartedButton(),
       ],
-    );
-  }
-
-  Widget _buildFeaturesList() {
-    return Column(
-      children: features
-          .map(
-            (feature) => FeatureItem(
-              icon: feature.icon,
-              title: feature.title,
-              description: feature.description,
-            ),
-          )
-          .toList(),
     );
   }
 
