90c23557fe38095980575177486b657efb9d93d6
kumarpalsihn25 <info@dajiraj.com>
2025-09-15 13:35:14 +0530

    Refactor BulletDetailScreen to integrate long tap functionality for text editing

--- lib/features/bullets/screens/bullet_detail_screen.dart
+++ lib/features/bullets/screens/bullet_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zoe/common/widgets/content_menu_button.dart';
+import 'package:zoe/common/widgets/long_tap_bottom_sheet.dart';
 import 'package:zoe/common/widgets/max_width_widget.dart';
 import 'package:zoe/common/widgets/paper_sheet_background_widget.dart';
 import 'package:zoe/common/widgets/quill_editor/widgets/quill_editor_positioned_toolbar_widget.dart';
@@ -110,49 +111,49 @@ class BulletDetailScreen extends ConsumerWidget {
     BulletModel bullet,
     bool isEditing,
   ) {
-    return GestureDetector(
-      onLongPress: () =>
-          ref.read(editContentIdProvider.notifier).state = bulletId,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: ZoeInlineTextEditWidget(
-                  hintText: L10n.of(context).title,
-                  isEditing: isEditing,
-                  text: bullet.title,
-                  textStyle: TextStyle(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.onSurface,
-                    height: 1.2,
-                  ),
-                  onTextChanged: (value) => ref
-                      .read(bulletListProvider.notifier)
-                      .updateBulletTitle(bulletId, value),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ZoeInlineTextEditWidget(
+                hintText: L10n.of(context).title,
+                isEditing: isEditing,
+                text: bullet.title,
+                textStyle: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onSurface,
+                  height: 1.2,
+                ),
+                onTextChanged: (value) => ref
+                    .read(bulletListProvider.notifier)
+                    .updateBulletTitle(bulletId, value),
+                onLongTapText: () => showLongTapBottomSheet(
+                  context,
+                  contentId: bulletId,
                 ),
               ),
-            ],
-          ),
-          const SizedBox(height: 16),
-          ZoeHtmlTextEditWidget(
-            hintText: L10n.of(context).addADescription,
-            isEditing: isEditing,
-            description: bullet.description,
-            textStyle: Theme.of(context).textTheme.bodyLarge,
-            editorId: 'bullet-description-$bulletId',
-            // Add unique editor ID
-            onContentChanged: (description) => Future.microtask(
-              () => ref
-                  .read(bulletListProvider.notifier)
-                  .updateBulletDescription(bulletId, description),
             ),
+          ],
+        ),
+        const SizedBox(height: 16),
+        ZoeHtmlTextEditWidget(
+          hintText: L10n.of(context).addADescription,
+          isEditing: isEditing,
+          description: bullet.description,
+          textStyle: Theme.of(context).textTheme.bodyLarge,
+          editorId: 'bullet-description-$bulletId',
+          // Add unique editor ID
+          onContentChanged: (description) => Future.microtask(
+            () => ref
+                .read(bulletListProvider.notifier)
+                .updateBulletDescription(bulletId, description),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
