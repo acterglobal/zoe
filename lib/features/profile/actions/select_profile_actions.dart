d1a11222a735cd8b2da28099360d24ee75a2288f
anisha-e10 <anisha.e10@dajiraj.com>
2025-09-26 11:39:53 +0530

    select profile action test cases added

--- lib/features/profile/actions/select_profile_actions.dart
+++ lib/features/profile/actions/select_profile_actions.dart
@@ -4,6 +4,8 @@ import 'package:image_picker/image_picker.dart';
 import 'package:zoe/common/widgets/document_selection_bottom_sheet.dart';
 import 'package:zoe/features/users/providers/user_providers.dart';
 
+final imagePickerProvider = Provider<ImagePicker>((ref) => ImagePicker());
+
 void selectProfileFileSource(
   BuildContext context,
   String userId,
@@ -13,14 +15,14 @@ void selectProfileFileSource(
   showFileSelectionBottomSheet(
     context,
     onTapCamera: () async {
-      final image = await _pickImage(ImageSource.camera);
+      final image = await _pickImage(ref, ImageSource.camera);
       if (image != null) {
         _updateUserAvatar(ref, userId, image.path);
         onImageSelected?.call(image.path);
       }
     },
     onTapGallery: () async {
-      final image = await _pickImage(ImageSource.gallery);
+      final image = await _pickImage(ref, ImageSource.gallery);
       if (image != null) {
         _updateUserAvatar(ref, userId, image.path);
         onImageSelected?.call(image.path);
@@ -29,8 +31,8 @@ void selectProfileFileSource(
   );
 }
 
-Future<XFile?> _pickImage(ImageSource source) async {
-  return ImagePicker().pickImage(
+Future<XFile?> _pickImage(WidgetRef ref, ImageSource source) async {
+  return ref.read(imagePickerProvider).pickImage(
     source: source,
     imageQuality: 80,
   );
