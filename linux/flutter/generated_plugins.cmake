2a91e2e05edc4aeceff727de58c8d21e4396fbc7
kumarpalsihn25 <info@dajiraj.com>
2025-12-12 15:03:05 +0530

    Remove zoe_native and flutter_secure_storage; add platform interfaces.

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -6,12 +6,11 @@ list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_linux
   emoji_picker_flutter
   file_selector_linux
-  flutter_secure_storage_linux
+  gtk
   url_launcher_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  zoe_native
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
