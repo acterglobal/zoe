9f7101672ef0305c78fffb2e2b93498c43965436
kumarpalsihn25 <info@dajiraj.com>
2025-12-16 17:19:26 +0530

    Refactor: Remove Links and Documents features
    Temporarily remove the "Links" and "Documents" features from the application. This includes:
    - Commenting out the "Links" and "Documents" stats widgets on the home screen.
    - Hiding the "Links" and "Documents" sections from the quick search screen and filters.
    - Removing the options to add new links or documents from the content menu.

--- lib/features/home/widgets/stats_section/stats_section_widget.dart
+++ lib/features/home/widgets/stats_section/stats_section_widget.dart
@@ -3,10 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:zoe/core/routing/app_routes.dart';
 import 'package:zoe/core/theme/colors/app_colors.dart';
-import 'package:zoe/features/documents/providers/document_providers.dart';
 import 'package:zoe/features/events/providers/event_providers.dart';
 import 'package:zoe/features/home/widgets/stats_section/stats_widget.dart';
-import 'package:zoe/features/link/providers/link_providers.dart';
 import 'package:zoe/features/polls/providers/poll_providers.dart';
 import 'package:zoe/features/sheet/providers/sheet_providers.dart';
 import 'package:zoe/features/task/providers/task_providers.dart';
@@ -20,8 +18,8 @@ class StatsSectionWidget extends ConsumerWidget {
     final sheetList = ref.watch(sheetListProvider);
     final eventList = ref.watch(eventListProvider);
     final taskList = ref.watch(taskListProvider);
-    final linkList = ref.watch(linkListProvider);
-    final documentList = ref.watch(documentListProvider);
+    // final linkList = ref.watch(linkListProvider);
+    // final documentList = ref.watch(documentListProvider);
     final pollList = ref.watch(pollListProvider);
 
     return Column(
@@ -66,19 +64,19 @@ class StatsSectionWidget extends ConsumerWidget {
             const SizedBox(width: 12),
             Expanded(
               child: StatsWidget(
-                icon: Icons.link_rounded,
-                count: linkList.length.toString(),
-                title: L10n.of(context).links,
-                color: Colors.blueAccent,
-                onTap: () => context.push(AppRoutes.linksList.route),
+                icon: Icons.poll_rounded,
+                count: pollList.length.toString(),
+                title: L10n.of(context).polls,
+                color: AppColors.brightMagentaColor,
+                onTap: () => context.push(AppRoutes.pollsList.route),
               ),
             ),
           ],
         ),
-        const SizedBox(height: 12),
+        // const SizedBox(height: 12),
 
         // Documents and Polls
-        Row(
+        /*Row(
           children: [
             Expanded(
               child: StatsWidget(
@@ -92,15 +90,15 @@ class StatsSectionWidget extends ConsumerWidget {
             const SizedBox(width: 12),
             Expanded(
               child: StatsWidget(
-                icon: Icons.poll_rounded,
-                count: pollList.length.toString(),
-                title: L10n.of(context).polls,
-                color: AppColors.brightMagentaColor,
-                onTap: () => context.push(AppRoutes.pollsList.route),
+                icon: Icons.link_rounded,
+                count: linkList.length.toString(),
+                title: L10n.of(context).links,
+                color: Colors.blueAccent,
+                onTap: () => context.push(AppRoutes.linksList.route),
               ),
             ),
           ],
-        ),
+        ),*/
       ],
     );
   }
